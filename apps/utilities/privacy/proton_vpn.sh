d2b5e8e4ae50600b3db8cee40429611f2359dd51
Patrick Boyle <t.patrick.boyle@pm.me>
2025-09-26 08:52:53 -0500

    + Proton VPN is self-sufficient lil baby now

--- apps/utilities/privacy/proton_vpn.sh
+++ apps/utilities/privacy/proton_vpn.sh
@@ -3,12 +3,20 @@
 source "src/packages/dpkg.sh"
 source "src/packages/apt/packages.sh"
 
-DEB_URL="https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.6_all.deb"
+DEB_URL="https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb"
+APT_PACKAGE_LIST_PATH="/var/lib/apt/lists/repo.protonvpn.com_debian_dists_stable_InRelease"
 
 ensure_proton_vpn_is_installed () {
   dpkg_ensure_package_is_installed "protonvpn-stable-release" "$DEB_URL"
+  ensure_proton_vpn_package_list_has_been_updated
   apt_ensure_package_is_installed proton-vpn-gnome-desktop
   ensure_additional_proton_vpn_utilities_are_installed
+}
+
+ensure_proton_vpn_package_list_has_been_updated () {
+  if [[ ! -e "$APT_PACKAGE_LIST_PATH" ]]; then
+    sudo apt update
+  fi
 }
 
 ensure_additional_proton_vpn_utilities_are_installed () {
